9a10816641462a8704174248d24b14e1dcfc3939
josejpalacios <jose.palacios6119@gmail.com>
2021-01-21 21:13:09 -0600

    Create Exercise 09: Select for CSS Selectors.py

---
+++ Module 01: Beautiful Soup/Lesson 01: Web Scraping with Beautiful Soup/Exercise 09: Select for CSS Selectors.py
@@ -0,0 +1,34 @@
+# .select(): Use CSS selectors to grab data.
+
+# Example: Grab all tags with class recipleLink
+# soup.select(".recipeLink")
+
+# Example: Grab all tags with id selected
+# soup.select("#selected")
+
+import requests
+from bs4 import BeautifulSoup
+
+prefix = "https://content.codecademy.com/courses/beautifulsoup/"
+webpage_response = requests.get('https://content.codecademy.com/courses/beautifulsoup/shellter.html')
+
+webpage = webpage_response.content
+soup = BeautifulSoup(webpage, "html.parser")
+
+turtle_links = soup.find_all("a")
+links = []
+#go through all of the a tags and get the links associated with them:
+for a in turtle_links:
+  links.append(prefix+a["href"])
+    
+#Define turtle_data:
+turtle_data = {}
+
+#follow each link:
+for link in links:
+  webpage = requests.get(link)
+  turtle = BeautifulSoup(webpage.content, "html.parser")
+  #Add your code here:
+  turtle_name = turtle.select(".name")[0]
+  # Add key value pair to dictionary | Set value to empty list
+  turtle_data[turtle_name] = []
